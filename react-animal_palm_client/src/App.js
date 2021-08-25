40fa2cf8b4c3725c302d3646fe5557727a2d080b
SEB-Full-30-Shim-jihyun <nsa06162@naver.com>
2021-08-25 11:28:46 +0900

    [Client] /#8 / feature/signup : signup skeleton

--- react-animal_palm_client/src/App.js
+++ react-animal_palm_client/src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import { Signup } from './Pages/Signup';
 
 function App() {
   return (
     <div className="App">
       {/* {체크할 컴포넌트} */}
+      <Signup />
     </div>
   );
 }
