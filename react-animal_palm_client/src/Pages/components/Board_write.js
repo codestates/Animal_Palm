3c8526005ba0749ddcf3bae6c79cdecc4b389f74
SEB-Full-30-Shim-jihyun <nsa06162@naver.com>
2021-08-26 16:48:57 +0900

    aa

--- react-animal_palm_client/src/Pages/components/Board_write.js
+++ react-animal_palm_client/src/Pages/components/Board_write.js
@@ -1,17 +1,47 @@
+import axios from 'axios';
+import { useState } from 'react';
 import '../Board.css'
 
      
      function Board_write() {
+
+      const [context,setContext] = useState({
+        title:'',
+        content:'',
+        hash:'',
+      })
+
+      const handleInputValue = (key) => (e) => {
+        setContext({...context, [key]: e.target.value })
+      }
+
+      const postContext = () => {
+        if(context.title===''|| context.content===''){
+          alert('빈칸을 입력해주세요')
+        }
+        else{
+        axios
+          .post(
+            `http://localhost:4000/boards/${1}`,
+            {
+              title: context.title,
+              content: context.content,
+              hash: context.hash
+            }
+          )
+          }
+      }
+
        return (
          
          <div className="Board_write">
              <h2>제목</h2>
-             <input placeholder="제목을 입력하세요" type="title"></input>
+             <input placeholder="제목을 입력하세요" type="title" onChange={handleInputValue('title')} ></input>
              <br></br>
-             <textarea placeholder="내용을 입력하세요"></textarea>
+             <textarea placeholder="내용을 입력하세요" onChange={handleInputValue('content')} ></textarea>
              <br></br>
-             <input placeholder="해쉬태그"></input>
-             <button type='submit'>완료</button>
+             <input placeholder="해쉬태그" onChange={handleInputValue('hash')} ></input>
+             <button type='submit' onClick={postContext} >완료</button>
          </div>
        );
      }
