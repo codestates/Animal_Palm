aad2b00a1f137948629deeab755dff3bd218e9bf
SEB-Full-30-Shim-jihyun <nsa06162@naver.com>
2021-08-27 04:16:57 +0900

    [Client] / #53 / feature/board : board ajax and state

--- react-animal_palm_client/src/Pages/Board.js
+++ react-animal_palm_client/src/Pages/Board.js
@@ -5,51 +5,11 @@ import Board_write from './components/Board_write';
 import Navigaitionbar from './components/navigationbar'
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import { useState } from 'react';
-import axios from 'axios';
 
 
-export function Board() {
-  const [context, setContext] = useState()
-  const [comment, setComment] = useState()
-  const [contentList, setContentList] = useState()
 
-  const loadContent = (id, contextId) => {
-    axios
-     .get(
-      `http://localhost:4000/boards/${id}/${contextId}`
-     ).then( data => {
-       setContext(data)
-     })
-  }
-
-  const loadComment = (id, postId) => {
-    axios
-     .get(
-       `http://localhost:4000/boards/${id}/${postId}`
-     ).then( data => {
-      setComment(data)
-     })
-  } 
-
-  const loadContentList = (id) => {
-    axios
-     .get(
-      `http://localhost:4000/boards/${id}`
-     ).then( data => {
-       setContentList(data)
-     })
-  }
-
-
-  const postComment = () => {
-    axios
-      .post(
-        `http://localhost:4000/comments/${1}`,
-        {
-          comment : 1
-        }
-      )
-  }
+export function Board({animalId}) {
+  animalId = 'animalId'
 
   return (
   <div>    
@@ -58,13 +18,13 @@ export function Board() {
     <div className="Board">
       <Switch>
         <Route exact path="/board">
-          <Board_list contentList={contentList} loadComment={loadComment} loadContent={loadContent} />
+          <Board_list animalId={animalId} />
         </Route>
         <Route path="/board/post">
-          <Board_post context={context} comment={comment} postComment={postComment} />
+          <Board_post animalId={animalId} />
         </Route>
         <Route path="/board/write">
-          <Board_write />
+          <Board_write animalId={animalId} />
         </Route>
       </Switch>
     </div>
