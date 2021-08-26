b9df24995a14bc20bae07dcaf166591422f37621
bonschicken <wjswls45645697@gmail.com>
2021-08-26 20:53:52 +0900

    [Server] / #3,#11,#15 / [router]: api router
    api 문서에 맞게 router ,[Comments],[Mypage] 논직 구현

--- Animal_palm_server/controllers/UserController.js
+++ Animal_palm_server/controllers/UserController.js
@@ -1,11 +1,32 @@
 module.exports ={
   signin: (req,res)=>{
+    //req.body에 id, pw 담겨있음
+    //-> DB에서 id, pw 일치하는 유저 있는지 찾아서
+    //-> 있으면 access token 주면서 return / 없으면 401
+
+    //token 관리 어떻게 할 건지 다시 이야기해보기
+    //access token에 id, mbti를 담아서 body로 주고
+    //  client에서 필요할 때마다 token을 이용해 유저 정보 받아가는 get 요청 필요하지 않을까?
+    //  client에서는 token 디코딩 못한다고 알고 있음
+    //  로그인할 때 token 받고 -> 성공시 받은 token으로 정보 요청해서 해석한 결과 돌려받기
+    //  이렇게 해서 받은 유저 정보 client에서 state로 관리
+    //refresh token에 id를 담아서 쿠키에 저장(만료기간 넉넉하게 생성)
+    //로그인상태 필요할 때마다 refresh token으로 access token 갱신
     return res.send('signin')
   },
   signup:(req,res)=>{
+    //req.body에 유저 정보 담겨있음
+    //-> DB에서 id 일치하는 유저 있는지 찾아서
+    //-> 있으면 409 없으면 가입완료
+    //findOrCreate로 탐색
     return res.send("signup")
   },
   signout:(req,res)=>{
+    //req로 client가 가지고 있던 token 받아옴
+    //유효한 token일 경우에만 로그아웃 진행
+    //  유효 여부 판별: 디코딩한 데이터가 DB에 포함되어 있는지 확인
+    //  DB에 포함되어있지 않은 data -> 401
+    //req.body로 받아온 access token, 쿠키에 담겨있던 refresh token 둘 다 삭제
     return res.send("signout")
   },
   userDelete:(req,res)=>{
--- Animal_palm_server/controllers/commentController.js
+++ Animal_palm_server/controllers/commentController.js
@@ -1,8 +1,30 @@
 module.exports ={
     comment: (req,res)=>{
+    //*GET method
+    //TODO: 현재 게시글의 댓글을 가져오는 method
+
+    //엔드포인트: http://localhost:4000/comments/:postid
+    //api book에 있는 엔드포인트에서 끝 /comment 부분은 불필요하다고 생각됨
+
+    //req.params에 담긴 현재 게시글의 id를 통해 탐색 가능
+    //comments.findAll({where: {post_Id = req.params.postId}})
+    //return값은 배열로 받게됨
+    //해당 배열(=result)을 map해서 client에서 사용 가능한 형태로 body에 담아 응답
         return res.send('comment')
     },
     writecomment:(req,res)=>{
+    //*POST method
+    //TODO: 현재 게시글에 댓글을 다는 method
+
+    //엔드포인트: http://localhost:4000/comments/:postid/
+
+    //req에 담긴 현재 게시글의 정보를 comments DB에 추가
+    //comments.create({post_id, content, user_id})
+    //postId: req.params, content: req.body, user_id: req.cookie(token에 담겨있음)
+
+    //token에 담긴 유저가 유효한 유저인지 항상 유효성부터 체크
+    //postId가 존재하는 게시글인지 확인 -> postDB에서 확인
+    //유효한 유저가 존재하는 게시글에 작성한 댓글일 경우에만 commentsDB에 추가
         return res.send("writecomment")
     },
     commentDelete:(req,res)=>{
