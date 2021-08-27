435bf6a6802ebca48ec7223ed56e2840a6b29889
seyoonkim48 <seyoonkim48@gmail.com>
2021-08-28 01:06:53 +0900

    [Client] / #49 / feat/mypage : Mycontent.js
    내가 작성한 글, 댓글 ajax 요청

--- react-animal_palm_client/src/Pages/components/MyComments.js
+++ react-animal_palm_client/src/Pages/components/MyComments.js
@@ -1,7 +1,10 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
 import { ContentsList } from './ContentsList';
 
-export const MyComments = () => {
+export const MyComments = ({
+  userInfo
+}) => {
   const testComments = [
     {id : 1, context : '테스트 댓글내용1', post_id: 1, created_At : '2021-08-25'},
     {id : 2, context : '테스트 댓글내용2', post_id: 2, created_At : '2021-08-25'},
@@ -10,6 +13,28 @@ export const MyComments = () => {
     {id : 5, context : '테스트 댓글내용5', post_id: 5, created_At : '2021-08-25'},
     {id : 6, context : '테스트 댓글내용6', post_id: 6, created_At : '2021-08-25'}
   ];
+  const [list, setList] = useState([]);
+  const getMyComment = () => {
+    const myCommentURL = 'http://3.34.133.160:4000/mypage/comment';
+    axios.get(myCommentURL)
+    .then((res) => {
+      const message = res.data.message;
+      if(message === 'ok') {
+        // keepGoing
+        setList(res.data.data);
+        if(list.length === 0) {
+          //! 작성한 댓글이 없습니다!!!!!!!
+          return;
+        }
+      } else {
+        alert('잘못된 접근입니다.')
+        return;
+      }
+    })
+  }
+  useEffect(() => {
+    getMyComment();
+  }, []);
   return (
     <>
       <div>
@@ -25,7 +50,7 @@ export const MyComments = () => {
           </tr>
         </thead>
         <tbody>
-          <ContentsList list={testComments}/>
+          <ContentsList list={list}/>
         </tbody>
         </table>
         : <div>작성한 댓글이 없습니다.</div>  
--- react-animal_palm_client/src/Pages/components/MyContents.js
+++ react-animal_palm_client/src/Pages/components/MyContents.js
@@ -1,12 +1,37 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
 import { ContentsList } from './ContentsList';
 
-export const MyContents = () => {
+export const MyContents = ({
+  userInfo
+}) => {
   const testlist = [
     {id: 1, category:'dog', context: '글제목1', created_At:'2021-08-25'},
     {id: 2, category:'All', context: '글제목2', created_At:'2021-08-25'},
     {id: 3, category:'dog', context: '글제목3', created_At:'2021-08-25'}
   ];
+  const [list, setList] = useState([]);
+  const getMyContent = () => {
+    const myContentURL = 'http://3.34.133.160:4000/mypage/context';
+    axios.get(myContentURL)
+    .then((res) => {
+      const message = res.data.message;
+      if(message === 'ok') {
+        // keep going
+        setList(res.data.data);
+        if(list.length === 0) {
+          // ! 작성한 글이 없습니다 보여주기 => state 하나 더 만들어서 이게 true면 글이 없습니다?
+          return;
+        }
+      } else {
+        alert('잘못된 접근입니다.')
+        return;
+      }
+    })
+  }
+  useEffect(() => {
+    getMyContent()
+  }, [])
   return (
     <>
       <div>
@@ -25,7 +50,9 @@ export const MyContents = () => {
                 </tr>
               </thead>
               <tbody>
-                <ContentsList list={testlist}/>
+                <ContentsList
+                  list={list}
+                />
               </tbody>
             </table>
           : <div>작성한 글이 없습니다.</div>
