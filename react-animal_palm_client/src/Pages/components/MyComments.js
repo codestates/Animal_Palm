525d6c35eb5cbc6ab520b923e857e0ab730cf8e4
seyoonkim48 <seyoonkim48@gmail.com>
2021-08-28 18:35:03 +0900

    [Client] / #69 / feat/mypage : Mypage.js
    delete 관련 ajax 요청 구현
    signout ajax 구현

--- react-animal_palm_client/src/Pages/components/MyComments.js
+++ react-animal_palm_client/src/Pages/components/MyComments.js
@@ -5,14 +5,14 @@ import { ContentsList } from './ContentsList';
 export const MyComments = ({
   userInfo
 }) => {
-  const testComments = [
-    {id : 1, context : '테스트 댓글내용1', post_id: 1, created_At : '2021-08-25'},
-    {id : 2, context : '테스트 댓글내용2', post_id: 2, created_At : '2021-08-25'},
-    {id : 3, context : '테스트 댓글내용3', post_id: 3, created_At : '2021-08-25'},
-    {id : 4, context : '테스트 댓글내용4', post_id: 4, created_At : '2021-08-25'},
-    {id : 5, context : '테스트 댓글내용5', post_id: 5, created_At : '2021-08-25'},
-    {id : 6, context : '테스트 댓글내용6', post_id: 6, created_At : '2021-08-25'}
-  ];
+  // const testComments = [
+  //   {id : 1, context : '테스트 댓글내용1', post_id: 1, created_At : '2021-08-25'},
+  //   {id : 2, context : '테스트 댓글내용2', post_id: 2, created_At : '2021-08-25'},
+  //   {id : 3, context : '테스트 댓글내용3', post_id: 3, created_At : '2021-08-25'},
+  //   {id : 4, context : '테스트 댓글내용4', post_id: 4, created_At : '2021-08-25'},
+  //   {id : 5, context : '테스트 댓글내용5', post_id: 5, created_At : '2021-08-25'},
+  //   {id : 6, context : '테스트 댓글내용6', post_id: 6, created_At : '2021-08-25'}
+  // ];
   const [list, setList] = useState([]);
   const getMyComment = () => {
     const myCommentURL = 'http://3.34.133.160:4000/mypage/comment';
@@ -32,25 +32,45 @@ export const MyComments = ({
       }
     })
   }
+  const deleteCommentHandler = (commentId) => {
+    const deleteCommentURL = `http://3.34.133.160:4000/comments/${commentId}/`
+    axios.delete(deleteCommentURL)
+    .then((res) => {
+      const message = res.data.message;
+      if(message === 'ok') {
+        alert('댓글이 삭제되었습니다.')
+        return;
+      } else {
+        alert('잘못된 접근입니다.')
+        return;
+      }
+    })
+    .catch((err) => alert(err))
+  }
   useEffect(() => {
     getMyComment();
-  }, []);
+  }, [list]);
   return (
     <>
       <div>
         내가 쓴 댓글
       </div>
-      {testComments.length > 0?
+      {list.length > 0?
         <table>
           <thead>
           <tr>
             <th>No.</th>
             <th>댓글 내용</th>
             <th>작성 일자</th>
+            <th>삭제</th>
           </tr>
         </thead>
         <tbody>
-          <ContentsList list={list}/>
+          <ContentsList
+            list={list}
+            show='comment'
+            handleDelete = {deleteCommentHandler}
+          />
         </tbody>
         </table>
         : <div>작성한 댓글이 없습니다.</div>  
--- react-animal_palm_client/src/Pages/components/MyContents.js
+++ react-animal_palm_client/src/Pages/components/MyContents.js
@@ -5,11 +5,11 @@ import { ContentsList } from './ContentsList';
 export const MyContents = ({
   userInfo
 }) => {
-  const testlist = [
-    {id: 1, category:'dog', context: '글제목1', created_At:'2021-08-25'},
-    {id: 2, category:'All', context: '글제목2', created_At:'2021-08-25'},
-    {id: 3, category:'dog', context: '글제목3', created_At:'2021-08-25'}
-  ];
+  // const testlist = [
+  //   {id: 1, category:'dog', context: '글제목1', created_At:'2021-08-25'},
+  //   {id: 2, category:'All', context: '글제목2', created_At:'2021-08-25'},
+  //   {id: 3, category:'dog', context: '글제목3', created_At:'2021-08-25'}
+  // ];
   const [list, setList] = useState([]);
   const getMyContent = () => {
     const myContentURL = 'http://3.34.133.160:4000/mypage/context';
@@ -39,7 +39,7 @@ export const MyContents = ({
           내가 쓴 글
         </div>
         <div>
-          {testlist.length > 0?
+          {list.length > 0?
             <table>
               <thead>
                 <tr>
@@ -47,11 +47,13 @@ export const MyContents = ({
                   <th>게시판</th>
                   <th>글 목록</th>
                   <th>작성 일자</th>
+                  <th>삭제</th>
                 </tr>
               </thead>
               <tbody>
                 <ContentsList
                   list={list}
+                  show='post'
                 />
               </tbody>
             </table>
