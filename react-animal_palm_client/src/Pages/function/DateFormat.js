b5b68cbef445a46418e39b03fb93c53f80ef8d3f
seyoonkim48 <seyoonkim48@gmail.com>
2021-09-03 10:06:34 +0900

    [Client]/#/ 날짜 포멧 수정

--- react-animal_palm_client/src/Pages/function/DateFormat.js
+++ react-animal_palm_client/src/Pages/function/DateFormat.js
@@ -1,4 +1,4 @@
 import moment from 'moment';
 export const getParsedDate = (createdAt) => {
-  return moment(createdAt).format('YY-MM-DD HH:MM');
+  return moment(createdAt).format('YY-MM-DD hh:mm');
 }
